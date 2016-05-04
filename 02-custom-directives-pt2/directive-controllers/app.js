2dc6b351fec82496951f269279b76867c17506e1
Wfuger <wfuger@gmail.com>
2016-05-04 11:46:38 -0600

    02 wip

---
+++ 02-custom-directives-pt2/directive-controllers/app.js
@@ -0,0 +1,23 @@
+var app = angular.module('circleApp', []);
+app.directive('gsBigRedCircle', function() {
+    return {
+        controller: function($scope) {
+            $scope.view = {};
+            $scope.view.sayHi = function() {
+                alert("Hi! Thanks for clicking on me!");
+            };
+        },
+        template: '<div class="circle">Click me!</div>',
+        link: function(scope, element, attrs) {
+            element.on('click', function() {
+                scope.view.sayHi();
+            });
+            element.on('mouseenter', function() {
+                element.css('background-color', 'blue');
+            });
+            element.on('mouseleave', function() {
+                element.css('background-color', 'red');
+            });
+        }
+    };
+});
